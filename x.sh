f28d8b9b49199813282d2d34241546c31433670b
John Reed <johnr@loogatee.com>
2018-05-31 14:20:02 -0700

    new x.sh, content changed to content_pub

--- x.sh
+++ x.sh
@@ -1,39 +1,36 @@
 #!/bin/bash
 
 
-rm -rf ./public
-hugo
+rm -rf ./docs
+hugo -c "./content_pub" -d "./docs"
 
-mkdir -p public/user9/img
-mkdir -p public/img
+mkdir -p docs/user9/img
 
 #
 #   Find all names beginning in cover_
 #   Copy them to 2 different directories:
-#       1.  ./public/img
-#       2.  ./public/user9/img
+#       1.  ./docs/img
+#       2.  ./docs/user9/docs/img
 #
 #   This works around some link-generation bugs in hugo
+#
+find content_pub -name cover_* -exec cp {} ./docs/img        \;
+find content_pub -name cover_* -exec cp {} ./docs/user9/img  \;
 
-find content -name cover_* -exec cp {} ./public/img              \;
-find content -name cover_* -exec cp {} ./public/user9/img        \;
-
-
-cp ./public/assets/img/README.txt ./public/README.md
-
-
-rm -rf docs
-mv public docs
-
-
-#git add .
-#git commit -m "auto-commit from x.sh.  Best to put a real message in here"
-#git push -u origin master
-
-
+cp ./docs/assets/img/README.txt ./docs/README.md
 
 #
-#sudo rm -rf /usr/share/monkey/mr/*
-#cd public; sudo rsync -avD * /usr/share/monkey/mr; cd ..
+# ==========================================
+#
 
+#rm -rf ./docs_priv
+#hugo -c "./content_priv" -d "./docs_priv"
+
+#mkdir -p docs_priv/user9/img
+
+#
+#find content_priv -name cover_* -exec cp {} ./docs_priv/img            \;
+#find content_priv -name cover_* -exec cp {} ./docs_priv/user9/img \;
+
+#cp ./docs_priv/img/README.txt ./docs_priv/README.md
 
