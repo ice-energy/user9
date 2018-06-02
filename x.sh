f413c59d867ff4f83d1cbb233da3ae8bd3c65d5d
John Reed <johnr@loogatee.com>
2018-06-02 14:51:06 -0700

    little typo in x.sh

--- x.sh
+++ x.sh
@@ -11,5 +11,5 @@ mv ./docs/img/README.txt ./docs/README.md
 
 rm -rf ./docs_priv
 hugo -c "./content_priv" -d "./docs_priv"
-mv ./docs_priv//img/README.txt ./docs_priv/README.md
+mv ./docs_priv/img/README.txt ./docs_priv/README.md
 
